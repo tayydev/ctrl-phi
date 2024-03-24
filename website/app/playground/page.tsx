1e478b3d6d1487a5a16158ec0974934c0b1e8cc7
Kieran Khan <kierank.pc@gmail.com>
2024-03-23 21:51:01 -0400

    playground update

--- website/app/playground/page.tsx
+++ website/app/playground/page.tsx
@@ -1,26 +1,160 @@
+'use client'
+
+import { ChangeEvent, FormEvent, FormEventHandler, useRef, useState } from "react";
 import Navbar from "../components/Navbar";
 import SimpleEditor from "../components/SimpleEditor";
-import { TextField } from "@mui/material";
+import { IconButton, InputAdornment, TextField } from "@mui/material";
+import Card from "../components/Card";
+import { exampleText } from "../util/exampleText";
+import SearchIcon from '@mui/icons-material/Search';
+
+interface Match {
+    explanation: string; // Example property
+    matched_text: string; // Example property
+    // Add other properties as needed
+  }
 
 export default function Playground() {
+    const [value, setValue] = useState('')
+    const [search, setSearch] = useState('')
+    const [matches, setMatches] = useState<Match[]>([])
+    const textFieldRef = useRef<HTMLTextAreaElement | null>(null)
+    
+    const handleValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value);
+      };
+    
+    const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearch(event.target.value);
+    };
+
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        handleApiRequest()
+    };
+
+    const highlightAndScrollToText = (searchText: string) => {
+        if(textFieldRef.current) {  
+            const input = textFieldRef.current
+            if (input != null) {
+                // Find the start index of the search text
+                const startIndex = input.value.indexOf(searchText);
+                if (startIndex !== -1) {
+                    const endIndex = startIndex + searchText.length;
+                    
+                    // Highlight the text
+                    input.focus();
+                    input.setSelectionRange(startIndex, endIndex);
+                    
+                    // Attempt to scroll to the highlighted text
+                    // Calculate the ratio of start index to the total length to estimate position
+                    const scrollRatio = startIndex / input.value.length;
+                    input.scrollTop = input.scrollHeight * scrollRatio - 20;
+                }
+            }
+        }
+        
+      };
+
+    const handleApiRequest = async () => {
+        const requestBody = {
+          query: "why did he die",
+          content: value // Replace this with the actual article content
+        };
+        try {
+            const response = await fetch('https://api.ctrl-phi.app/api', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'User-Agent': 'insomnia/8.6.1', // User-Agent header might not be allowed by browsers for security reasons
+              },
+              body: JSON.stringify(requestBody),
+            });
+      
+            if (!response.ok) {
+              throw new Error(`Error: ${response.status}`);
+            }
+      
+            const data = await response.json();
+            console.log(data.results)
+            setMatches(data.results)
+        } catch (error) {
+            console.error("Failed to fetch data:", error);
+          }
+    }
+
     return (
-        <div className="flex flex-col min-h-screen items-center justify-center bg-white">
-            <div className="absolute top-0 w-screen">
+        <div className="flex flex-col min-h-screen items-center bg-white">
+            <div className="top-0 w-screen">
                 <Navbar />
             </div>
-            <div className="md:w-1/2 sm:w-5/6 flex justify-center">
-                {/* <SimpleEditor /> */}
-                <TextField
-                    id="filled-multiline-static"
-                    label="Text Input"
-                    multiline
-                    rows={20}
-                    maxRows={1000}
-                    defaultValue=""
-                    variant="filled"
-                    sx={{width: '100%'}}
-                />
+            <div className="hidden md:flex flex-row w-screen items-center justify-center"> 
+                <div className="md:w-1/2 w-11/12 flex justify-center pt-8">
+                    {/* <SimpleEditor /> */}
+                    <TextField
+                        id="filled-multiline-static"
+                        label="Text Input"
+                        multiline
+                        rows={20}
+                        maxRows={1000}
+                        value={value}
+                        variant="filled"
+                        sx={{width: '100%'}}
+                        onChange={handleValueChange}
+                        inputRef={textFieldRef}
+                    />
+                </div>
+                <div className="px-10 justify-start items-center h-max flex flex-col mt-10 w-max">
+                    <form onSubmit={handleSubmit} className="w-max">
+                        <TextField
+                            label="Search Text"
+                            InputProps={{
+                                endAdornment: (
+                                <InputAdornment position="start">
+                                    <IconButton onClick={handleSubmit}>
+                                    <SearchIcon />
+                                    </IconButton>
+                                </InputAdornment>
+                                )
+                            }}
+                            onChange={handleSearchChange}
+                            onSubmit={() => console.log("PEEPEE")}
+                            rows={1}
+                        />
+                    </form>  
+                    {matches.map((value) => (
+                    <div className="py-3">
+                        <button onClick={(e) => highlightAndScrollToText(value.matched_text)} className="block max-w-sm p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
+                            <h5 className="mb-1 text-md font-semibold tracking-tight text-gray-900">{`"${value.matched_text}"`}</h5>
+                            <h5 className="mb-1 text-md tracking-tight text-gray-700">{`${value.explanation}`}</h5>
+                        </button>
+                    </div>
+
+                ))}
+                </div>
+                
             </div>
+            <div className="hidden md:flex flex-row">
+                <div className='p-5'>
+                    <button onClick={() => setValue(exampleText[0])} className="block max-w-xs p-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Alan Turing</h5>
+                    </button>
+                </div>
+                <div className='p-5'>
+                    <button onClick={() => setValue(exampleText[1])} className="block max-w-xs p-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Scholarly AI Article</h5>
+                    </button>
+                </div>
+                <div className='p-5'>
+                    <button onClick={() => setValue(exampleText[1])} className="block max-w-xs p-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Scholarly AI Article</h5>
+                    </button>
+                </div>
+            </div>
+            <div className="flex md:hidden">
+                <h1 className="text-md text-black font-mono font-semibold py-5">Playground not available on mobile!</h1>
+            </div>
+            
         </div>
     );
   }
