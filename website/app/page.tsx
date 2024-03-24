f727fe880e3adb3d590f701ebe66acf22d23d09d
Kieran Khan <kierank.pc@gmail.com>
2024-03-24 03:15:42 -0400

    playground updates

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -3,11 +3,11 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center">
+    <div className="flex flex-col min-h-screen items-center justify-center bg-white">
       <div className="absolute top-0 w-screen">
         <Navbar />
       </div>
-      <h1 className="text-4xl text-white font-semibold py-5">Control-F, Enhanced With AI</h1>
+      <h1 className="text-4xl text-black font-semibold py-5">Control-F, Enhanced With AI</h1>
       <button className="bg-violet-600 hover:bg-violet-800 text-white font-bold py-3 px-6 rounded text-3xl">
         Install
       </button>
