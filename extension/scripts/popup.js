4380d5dd0b5116af26af8ba99e102270761bbebd
tayydev <51377975+tayydev@users.noreply.github.com>
2024-03-23 20:21:08 -0400

    Initial extension work by eric

---
+++ extension/scripts/popup.js
@@ -0,0 +1,34 @@
+// Summary: popup HTML loads, getCurrentTab called (getting tab), scrapeHtml called (scraping text), set message to text
+
+// run when the popup's HTML has completely loaded
+document.addEventListener('DOMContentLoaded', function() {
+    getCurrentTabHtml().then(html => {
+        document.querySelector('#message').textContent = html;
+    }).catch(error => {
+        document.querySelector('#message').textContent = 'Error: ' + error.message;
+    });
+});
+
+async function getCurrentTabHtml() {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    if (!tab) {
+        throw new Error('No active tab found');
+    }
+
+    // injects scrapeHtml function into active tab
+    try {
+        let [result] = await chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: scrapeHtml,
+        });
+        return result.result;
+    } catch (error) {
+        console.error('Error executing script in the current tab:', error);
+        throw error;
+    }
+}
+
+function scrapeHtml() {
+    return document.documentElement.innerText; // returns text within HTML
+}
