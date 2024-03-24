a92939315a4f0cc7a705e5f463214bac8a7013a7
tayydev <51377975+tayydev@users.noreply.github.com>
2024-03-23 23:49:58 -0400

    All github pages integration

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
-    distDir: '../docs',
-    assetPrefix: "/nextassets"
   };
   
 export default nextConfig;
