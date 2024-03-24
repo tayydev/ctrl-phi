7f928d0c5eb89b16e6af005819171231f7c1dfdb
tayydev <51377975+tayydev@users.noreply.github.com>
2024-03-24 04:53:55 +0000

    Deploying to gh-pages from @ tayydev/ctrl-phi@52b77d883b4f01851ca92031f6bab0e7e7fccc7c 🚀

--- prompting/ResultModel.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class ResultModel(BaseModel):
-    matched_text: str
-    explanation: str
--- website/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-    distDir: '../docs',
-    assetPrefix: "/nextassets"
-  };
-  
-export default nextConfig;
